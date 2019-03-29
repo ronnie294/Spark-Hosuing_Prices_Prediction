cd8dd9f401d220e5bc6d42517bb496ac818fe4c3
utkarshpujari <pujari.u@husky.neu.com>
2019-03-29 18:44:06 -0400

    Record class outside, functions for parsing int and double

--- src/main/scala/com/mr/spark/housingprice/housingPrediction.scala
+++ src/main/scala/com/mr/spark/housingprice/housingPrediction.scala
@@ -11,90 +11,7 @@ import org.apache.spark.sql.functions.col
 
 object housingPrediction {
 
-  case class Record(
 
-                     MSSubclass: Double,
-                     MSZoning: String,
-                     LotFrontage: Double,
-                     LotArea: Double,
-                     Street: String,
-                     Alley: String,
-                     LotShape: String,
-                     LandContour: String,
-                     Utilities: String,
-                     LotConfig: String,
-                     LandSLope: String,
-                     Neighbourhood: String,
-                     Condition1: String,
-                     Condition2: String,
-                     BldgType: String,
-                     HouseStyle: String,
-                     OverallQual: Int,
-                     OverallCond: Int,
-                     YearBuilt: Int,
-                     YearRemodAdd: Int,
-                     RoofStyle: String,
-                     RoofMatl: String,
-                     Exterior1st: String,
-                     Exterior2nd: String,
-                     MasVnrType: String,
-                     MasVnrArea: String,
-                     ExterQual: String,
-                     ExterCond: String,
-                     Foundation: String,
-                     BsmtQual: String,
-                     BsmtCond: String,
-                     BsmtExposure: String,
-                     BsmtFinType1: String,
-                     BsmtFinSF1: Double,
-                     BsmtFinType2: String,
-                     BsmtFinSF2: Double,
-                     BsmtUnfSF: Double,
-                     TotalBsmtSF: Double,
-                     Heating: String,
-                     HeatingQC: String,
-                     CentralAir: String,
-                     Electrical: String,
-                     FirstFlrSF: Double,
-                     SecondFlrSF: Double,
-                     LowQualFinSF: Double,
-                     GrLivArea: Double,
-                     BsmtFullBath: Int,
-                     BsmtHalfBath: Int,
-                     FullBath: Int,
-                     HalfBath: Int,
-                     BedroomAbvGr: Int,
-                     KitchenAbvGr: Int,
-                     KitchenQual: String,
-                     TotRmsAbvGrd: Int,
-                     Functional: String,
-                     Fireplaces: Int,
-                     FireplaceQu: String,
-                     GarageType: String,
-                     GarageYrBlt: Int,
-                     GarageFinish: String,
-                     GarageCars: Int,
-                     GarageArea: Double,
-                     GarageQual: String,
-                     GarageCond: String,
-                     PavedDrive: String,
-                     WoodDeckSF: Double,
-                     OpenPorchSF: Double,
-                     EnclosedPorch: Double,
-                     SsnPorch: Double,
-                     ScreenPorch: Double,
-                     PoolArea: Double,
-                     PoolQC: String,
-                     Fence: String,
-                     MiscFeature: String,
-                     MiscVal: String,
-                     MoSold: Int,
-                     YrSold: Int,
-                     SaleType: String,
-                     SaleCondition: String,
-                     id: Int,
-                     SalePrice: Double
-                   )
 
   def main(args: Array[String]) {
 
@@ -104,7 +21,7 @@ object housingPrediction {
       logger.error("Usage:\nwc.WordCountMain <input dir> <output dir>")
       System.exit(1)
     }
-    val conf = new SparkConf().setAppName("Word Count")
+    val conf = new SparkConf().setAppName("Housing Price Prediction")
     val sc = new SparkContext(conf)
     val sparkSession = SparkSession.builder.
       master("local")
@@ -115,11 +32,6 @@ object housingPrediction {
 
 
 
-    /* val textFile = sc.textFile(args(0))
-     val counts = textFile.flatMap(line => line.split(" "))
-       .map(word => (word, 1))
-       .reduceByKey(_ + _)
-     counts.saveAsTextFile(args(1))*/
 
     var inputDataRDD = sc.textFile(args(0) + "/train.csv")
     var testDataRDD = sc.textFile(args(0) + "/test.csv")
@@ -130,41 +42,49 @@ object housingPrediction {
     testDataRDD = testDataRDD.filter(row => row!=testHeader)
     val inputDataSplitRDD = inputDataRDD.map(line => line.split(","))
     val testDataSplitRDD = testDataRDD.map(line => line.split(","))
-    val inputDataMapRDD = inputDataSplitRDD.map(p => Record(p(0).toDouble, p
-    (1), if(p(2)!="NA") p(2).toDouble else 0.0, p(3).toDouble, p(4), p(5), p(6), p(7), p(8), p(9),
+    def parseInt(a:String): Int = {
+      if(a!="NA")
+        return a.toInt
+      return 0
+    }
+    def parseDouble(a:String): Double = {
+      if(a!="NA")
+        return a.toDouble
+      return 0.0
+    }
+    val inputDataMapRDD = inputDataSplitRDD.map(p => Record(parseDouble(p(0)), p
+    (1), parseDouble(p(2)), parseDouble(p(3)), p(4), p(5), p(6), p(7), p(8), p(9),
       p(10), p(11), p(12), p(13), p(14), p(15), p(16).toInt, p(17).toInt, p
       (18).toInt, p(19).toInt,
       p(20), p(21), p(22), p(23), p(24), p(25), p(26), p(27), p(28), p(29),
-      p(30), p(31), p(32), p(33).toDouble, p(34), p(35).toDouble, p(36)
-        .toDouble, p(37).toDouble, p(38), p(39),
-      p(40), p(41), p(42).toDouble, p(43).toDouble, p(44).toDouble, p(45)
-        .toDouble, p(46).toInt, p(47).toInt, p(48).toInt, p(49).toInt,
+      p(30), p(31), p(32),parseDouble(p(33)), p(34), parseDouble(p(35)), parseDouble(p(36)), parseDouble(p(37)), p(38), p(39),
+      p(40), p(41), parseDouble(p(42)), parseDouble(p(43)), parseDouble(p(44)), parseDouble(p(45)), parseInt(p(46)), p(47).toInt, p(48).toInt, p(49).toInt,
       p(50).toInt, p(51).toInt, p(52), p(53).toInt, p(54), p(55).toInt, p(56)
-      , p(57), if(p(58)!="NA") p(58).toInt else 0, p(59),
+      , p(57), parseInt(p(58)), p(59),
       p(60).toInt, p(61).toDouble, p(62), p(63), p(64), p(65).toDouble, p(66)
         .toDouble, p(67).toDouble, p(68).toDouble, p(69).toDouble,
       p(70).toDouble, p(71), p(72), p(73), p(74), p(75).toInt, p(76).toInt, p
       (77), p(78), p(79).toInt, p(80).toDouble))
 
     val testDataMapRDD = testDataSplitRDD.map(p => Record(p(0).toDouble, p
-    (1), if(p(2)!="NA") p(2).toDouble else 0.0, p(3).toDouble, p(4), p(5), p(6), p(7), p(8), p(9),
+    (1), parseDouble(p(2)), p(3).toDouble, p(4), p(5), p(6), p(7), p(8), p(9),
       p(10), p(11), p(12), p(13), p(14), p(15), p(16).toInt, p(17).toInt, p
       (18).toInt, p(19).toInt,
       p(20), p(21), p(22), p(23), p(24), p(25), p(26), p(27), p(28), p(29),
-      p(30), p(31), p(32), if(p(33)!="NA") p(33).toDouble else 0.0, p(35),if(p(35)!="NA") p(35).toDouble else 0.0,if(p(36)!="NA") p(36).toDouble else 0.0,
-      if(p(37)!="NA") p(37).toDouble else 0.0, p(38), p(39),
+      p(30), p(31), p(32), parseDouble(p(33)), p(35),parseDouble(p(36)),parseDouble(p(37)),
+      parseDouble(p(37)), p(38), p(39),
       p(40), p(41), p(42).toDouble, p(43).toDouble, p(44).toDouble, p(45)
-        .toDouble,  if(p(46)!="NA") p(46).toInt else 0,  if(p(47)!="NA") p(47).toInt else 0,  if(p(48)!="NA") p(48).toInt else 0,  if(p(49)!="NA") p(49).toInt else 0,
+        .toDouble,  parseInt(p(46)),  parseInt(p(47)),  parseInt(p(48)),  parseInt(p(49)),
       p(50).toInt, p(51).toInt, p(52), p(53).toInt, p(54), p(55).toInt, p(56)
-      , p(57), if(p(58)!="NA") p(58).toInt else 0, p(59),
-      if(p(60)!="NA") p(60).toInt else 0,if(p(61)!="NA") p(61).toDouble else 0.0, p(62), p(63), p(64), if(p(65)!="NA") p(65).toDouble else 0.0, p(66)
+      , p(57), parseInt(p(58)), p(59),
+      parseInt(p(60)),parseDouble(p(61)), p(62), p(63), p(64), parseDouble(p(65)), p(66)
         .toDouble, p(67).toDouble, p(68).toDouble, p(69).toDouble,
       p(70).toDouble, p(71), p(72), p(73), p(74), p(75).toInt, p(76).toInt, p
       (77), p(78), p(79).toInt,0.0))
 
     val ecommDF = inputDataMapRDD.toDF()
     val testecommDF = testDataMapRDD.toDF()
-    print("utark"+ecommDF.head(10))
+    //print("utark"+ecommDF.head(10))
 
     val ecommDF1 = ecommDF.select(ecommDF("SalePrice").as("label"),
       $"MSSubClass",$"LotFrontage", $"LotArea", $"OverallQual",
